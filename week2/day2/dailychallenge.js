f29074fe58d43513b1747d2f9a7a048c55d8bd81
DavidKyng <davidschreiber6000@gmail.com>
2022-10-19 16:20:11 +0300

    daily challenge 19/10/22

---
+++ week2/day2/dailychallenge.js
@@ -0,0 +1,16 @@
+const sentence = "this is not that bad"
+const wordNot = sentence.indexOf("not")
+const wordBad = sentence.indexOf("bad")
+
+console.log(wordNot)
+console.log(wordBad)
+
+let a = sentence.substring(0, wordNot)
+let b = sentence.substring(wordBad + 3, sentence.length)
+
+if (wordBad > wordNot) {
+    console.log(a + "good" + b) 
+}
+else {
+    console.log(sentence)
+}
